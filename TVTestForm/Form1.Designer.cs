845d9c775a63de4c69631a69531f9dd8d4099ecb
nikiteezy <nikiteezy@gmail.com>
2017-12-16 17:33:00 +0300

    first files

---
+++ TVTestForm/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TVTestForm
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameCoinFirstLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameCoinFirstLbl
+            // 
+            this.nameCoinFirstLbl.AutoSize = true;
+            this.nameCoinFirstLbl.Location = new System.Drawing.Point(47, 32);
+            this.nameCoinFirstLbl.Name = "nameCoinFirstLbl";
+            this.nameCoinFirstLbl.Size = new System.Drawing.Size(35, 13);
+            this.nameCoinFirstLbl.TabIndex = 0;
+            this.nameCoinFirstLbl.Text = "label1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 985);
+            this.Controls.Add(this.nameCoinFirstLbl);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crypto Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameCoinFirstLbl;
+    }
+}
+
---
+++ TVTestForm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace TVTestForm
+{
+    public partial class FormMain : Form
+    {
+        private static string link_getMarketSummary = "https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCoinInfo("xem");
+            
+        }
+        void getCoinInfo(string coin)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string response = wc.DownloadString(link_getMarketSummary + coin);
+                var jStr = JsonConvert.DeserializeObject<dynamic>(response);
+                decimal last = jStr.result[0].Last;
+                nameCoinFirstLbl.Text = "" + last;
+            }
+        }
+        //update
+        #region
+
+        #endregion
+    }
+}
